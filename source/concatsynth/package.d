c03562a20ea9ae1c628c46136a4cc2f2a807ed20
LunaTheFoxgirl <cli-luna@protonmail.com>
2021-12-23 00:37:12 +0100

    boilerplate

---
+++ source/concatsynth/package.d
@@ -0,0 +1,11 @@
+/**
+    Module using concatenative synthesis to splice a pre-existing voice together
+    TODO: implement support for the UTAU format?
+
+    Copyright © 2020, Luna Nielsen
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module concatsynth;
+
---
+++ source/simsynth/package.d
@@ -0,0 +1,11 @@
+/**
+    Module attempting to roughly simulate a voice through various types of modulation
+    as well as formant filtering, noise generation, etc.
+
+    Copyright © 2020, Luna Nielsen
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module simsynth;
+
